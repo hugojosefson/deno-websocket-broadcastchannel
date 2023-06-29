055e4f5c8d8b05965a6bdcabc13cf3a7aec57377
Hugo Josefson <hugo@josefson.org>
2023-06-29 23:08:50 +0200

    feat: optional url argument to createBroadcastChannel
    Fixes #1

--- src/create-broadcast-channel.ts
+++ src/create-broadcast-channel.ts
@@ -4,19 +4,22 @@ import {
   BroadcastChannelIsh,
   GlobalThisWithBroadcastChannel,
 } from "./types.ts";
+import { DEFAULT_WEBSOCKET_URL } from "../mod.ts";
 
 /**
  * Creates a {@link BroadcastChannel} or {@link WebSocketBroadcastChannel}, depending on
  * whether we are running on Deno Deploy or not.
  * @param name The name of the channel.
+ * @param url WebSocket url to connect to or listen as, if not on Deno Deploy.
  */
 export async function createBroadcastChannel(
   name: string,
+  url: string | URL = DEFAULT_WEBSOCKET_URL,
 ): Promise<BroadcastChannelIsh> {
   if (await isDenoDeploy()) {
     const g = globalThis as GlobalThisWithBroadcastChannel;
     return new g.BroadcastChannel(name) as BroadcastChannelIsh;
   }
 
-  return new WebSocketBroadcastChannel(name);
+  return new WebSocketBroadcastChannel(name, url);
 }
