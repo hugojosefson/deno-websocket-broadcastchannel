d4b4fb6d0e55ac573d066542164292412aea425f
Hugo Josefson <hugo@josefson.org>
2023-07-02 13:19:41 +0200

    test: using

---
+++ src/using.ts
@@ -0,0 +1,70 @@
+const SymbolWithDispose = Symbol as
+  & SymbolConstructor
+  & {
+    readonly dispose: unique symbol;
+    readonly asyncDispose: unique symbol;
+  };
+
+type Disposable =
+  | { [SymbolWithDispose.dispose](): void }
+  | { [SymbolWithDispose.asyncDispose]: () => Promise<void> };
+
+export type Resource =
+  | Deno.Closer
+  | Disposable;
+
+export type ResourceFactory<R extends Resource> = () => R | Promise<R>;
+
+/**
+ * Similar to TC39 proposal for `using` blocks.
+ *
+ * Give it a bunch of functions that each create a resource, and a function that uses those resources.
+ * It will call the resource-creating functions, and pass the resources to the using function.
+ * Then "finally" it will [Symbol.dispose]/[Symbol.asyncDispose]/close all the resources.
+ */
+export async function using<
+  R extends Resource,
+  Rs extends R[] & { length: N },
+  N extends number,
+>(
+  resourceFactories: ResourceFactory<R>[] & { length: N },
+  fn: (resources: Rs) => void | Promise<void>,
+): Promise<void> {
+  return await recursiveUsing(resourceFactories, fn, []);
+}
+
+async function recursiveUsing<
+  R extends Resource,
+  Rs extends R[] & { length: N },
+  N extends number,
+>(
+  resourceFactories: ResourceFactory<R>[] & { length: N },
+  fn: (resources: Rs) => void | Promise<void>,
+  resources: R[],
+): Promise<void> {
+  if (resourceFactories.length === 0) {
+    return fn(resources as Rs);
+  }
+  const [head, ...tail]: ResourceFactory<R>[] & { length: N } =
+    resourceFactories;
+  const resource = await head();
+  try {
+    try {
+      try {
+        return await recursiveUsing(tail, fn, [...resources, resource]);
+      } finally {
+        if (typeof SymbolWithDispose.dispose === "symbol" && SymbolWithDispose.dispose in resource) {
+          resource[SymbolWithDispose.dispose]();
+        }
+      }
+    } finally {
+      if (typeof SymbolWithDispose.asyncDispose === "symbol" && SymbolWithDispose.asyncDispose in resource) {
+        await resource[SymbolWithDispose.asyncDispose]();
+      }
+    }
+  } finally {
+    if ("close" in resource) {
+      resource.close();
+    }
+  }
+}
--- test/websocket-broadcastchannel.test.ts
+++ test/websocket-broadcastchannel.test.ts
@@ -5,6 +5,8 @@ import {
 } from "https://deno.land/std@0.192.0/testing/bdd.ts";
 import {
   assertEquals,
+  assertInstanceOf,
+  assertNotStrictEquals,
   assertStrictEquals,
 } from "https://deno.land/std@0.192.0/testing/asserts.ts";
 import { createBroadcastChannel } from "../src/create-broadcast-channel.ts";
@@ -12,6 +14,8 @@ import { getAvailablePort } from "./get-available-port.ts";
 import { defaultWebSocketUrl } from "../src/default-websocket-url.ts";
 import { rejectOnTimeout } from "./fn.ts";
 import { deferred } from "https://deno.land/std@0.192.0/async/deferred.ts";
+import { using } from "../src/using.ts";
+import { WebSocketBroadcastChannel } from "../mod.ts";
 
 let url: URL;
 beforeAll(() => {
@@ -19,65 +23,71 @@ beforeAll(() => {
 });
 
 describe("websocket-broadcastchannel", () => {
-  describe("two instances in same process", () => {
-    it("should create them", async () => {
-      const bc1 = await createBroadcastChannel("chat", url);
-      try {
-        const bc2 = await createBroadcastChannel("chat", url);
-        bc2.close();
-      } finally {
-        bc1.close();
-      }
+  describe("same channel name", () => {
+    describe("two instances in same process", () => {
+      it("should create them", async () => {
+        await using(
+          [
+            () => createBroadcastChannel("chat", url),
+            () => createBroadcastChannel("chat", url),
+          ],
+          ([bc0, bc1]) => {
+            assertInstanceOf(bc0, WebSocketBroadcastChannel);
+            assertInstanceOf(bc1, WebSocketBroadcastChannel);
+            assertNotStrictEquals(bc0, bc1);
+          },
+        );
+      });
+      it("should send one message from each, to the other", async () => {
+        await using(
+          [
+            () => createBroadcastChannel("chat", url),
+            () => createBroadcastChannel("chat", url),
+          ],
+          async ([bc0, bc1]) => {
+            const receivedPromise0 = new Promise<string>((resolve) => {
+              bc0.onmessage = (e) => resolve(e.data);
+            });
+            const receivedPromise1 = new Promise<string>((resolve) => {
+              bc1.onmessage = (e) => resolve(e.data);
+            });
+            bc0.postMessage("test from bc0");
+            bc1.postMessage("test from bc1");
+            const [received0, received1] = await rejectOnTimeout([
+              receivedPromise0,
+              receivedPromise1,
+            ]);
+            assertStrictEquals(received0, "test from bc1");
+            assertStrictEquals(received1, "test from bc0");
+          },
+        );
+      });
     });
-    it("should send one message from each, to the other", async () => {
-      const bc1 = await createBroadcastChannel("chat", url);
-      try {
-        const bc2 = await createBroadcastChannel("chat", url);
-        try {
-          const receivedPromise1 = new Promise<string>((resolve) => {
-            bc1.onmessage = (e) => resolve(e.data);
-          });
-          const receivedPromise2 = new Promise<string>((resolve) => {
-            bc2.onmessage = (e) => resolve(e.data);
-          });
-          bc1.postMessage("test1");
-          bc2.postMessage("test2");
-          const [received1, received2] = await rejectOnTimeout([
-            receivedPromise1,
-            receivedPromise2,
-          ]);
-          assertStrictEquals(received1, "test2");
-          assertStrictEquals(received2, "test1");
-        } finally {
-          bc2.close();
-        }
-      } finally {
-        bc1.close();
-      }
-    });
-  });
-  describe("three instances in same process", () => {
-    it("should create them", async () => {
-      const bc1 = await createBroadcastChannel("chat", url);
-      try {
-        const bc2 = await createBroadcastChannel("chat", url);
-        try {
-          const bc3 = await createBroadcastChannel("chat", url);
-          bc3.close();
-        } finally {
-          bc2.close();
-        }
-      } finally {
-        bc1.close();
-      }
-    });
-    it("should send one message from each, to the others", async () => {
-      const bc0 = await createBroadcastChannel("chat", url);
-      try {
-        const bc1 = await createBroadcastChannel("chat", url);
-        try {
-          const bc2 = await createBroadcastChannel("chat", url);
-          try {
+    describe("three instances in same process", () => {
+      it("should create them", async () => {
+        await using(
+          [
+            () => createBroadcastChannel("chat", url),
+            () => createBroadcastChannel("chat", url),
+            () => createBroadcastChannel("chat", url),
+          ],
+          ([bc0, bc1, bc2]) => {
+            assertInstanceOf(bc0, WebSocketBroadcastChannel);
+            assertInstanceOf(bc1, WebSocketBroadcastChannel);
+            assertInstanceOf(bc2, WebSocketBroadcastChannel);
+            assertNotStrictEquals(bc0, bc1);
+            assertNotStrictEquals(bc0, bc2);
+          },
+        );
+      });
+      it("should send one message from each, to the others", async () => {
+        await using(
+          [
+            () => createBroadcastChannel("chat", url),
+            () => createBroadcastChannel("chat", url),
+            () => createBroadcastChannel("chat", url),
+          ],
+          async ([bc0, bc1, bc2]) => {
             const doneDeferred = deferred<void>();
             const expectedCount = 6;
             let receivedCount = 0;
@@ -104,15 +114,12 @@ describe("websocket-broadcastchannel", () => {
               ["test0", "test2"],
               ["test0", "test1"],
             ]);
-          } finally {
-            bc2.close();
-          }
-        } finally {
-          bc1.close();
-        }
-      } finally {
-        bc0.close();
-      }
+          },
+        );
+      });
     });
+  });
+  describe("2 instances w/ channel name 'chat2' and 3 instances w/ channel name 'chat3'", () => {
+    it("should send one message from each, to the others", async () => {});
   });
 });
